f585a9a8cab255259429a9ed9f9cb11e7a715f3a
MoatazHussein <Moatazhussein432@gmail.com>
2025-04-28 23:22:56 +0300

    Add project files.

---
+++ Restaurants.API/Controllers/RestaurantsController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.AspNetCore.Mvc;
+using Restaurants.Application.Restaurants;
+using Restaurants.Application.Restaurants.Dtos;
+using Restaurants.Domain.Entities;
+
+namespace Restaurants.API.Controllers
+{
+    [ApiController]
+    [Route("api/restaurants")]
+    public class RestaurantsController(IRestaurantsService restaurantsService) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var restaurants = await restaurantsService.GetAllRestaurants();
+            return Ok(restaurants);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Restaurant>> GetById([FromRoute] int id)
+        {
+            var restaurant = await restaurantsService.GetById(id);
+            if (restaurant is null)
+                return NotFound();
+
+            return Ok(restaurant);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRestaurant(CreateRestaurantDto createRestaurantDto)
+        {
+            int id = await restaurantsService.Create(createRestaurantDto);
+
+            return CreatedAtAction(nameof(GetById), new { id }, null);
+        }
+    }
+}
---
+++ Restaurants.Application/Restaurants/Validators/CreateRestaurantDtoValidator.cs
@@ -0,0 +1,35 @@
+﻿using FluentValidation;
+using Restaurants.Application.Restaurants.Dtos;
+
+namespace Restaurants.Application.Restaurants.Validators;
+
+public class CreateRestaurantDtoValidator : AbstractValidator<CreateRestaurantDto>
+{
+    private readonly List<string> validCategories = ["Italian", "Mexican", "Japanese", "American", "Indian"];
+    public CreateRestaurantDtoValidator()
+    {
+        RuleFor(dto => dto.Name).
+            NotEmpty().WithMessage("Please provide a valid name")
+            .Length(2, 50);
+
+        RuleFor(dto => dto.Category)
+        //.Must(category => validCategories.Contains(category))
+        //.WithMessage("Invalid category. Please choose from the valid categories.");
+        .Custom((value, context) =>
+        {
+            var isValidCategory = validCategories.Contains(value);
+            if (!isValidCategory)
+            {
+                context.AddFailure("Category", "Invalid category. Please choose from the valid categories.");
+            }
+        });
+
+        RuleFor(dto => dto.ContactEmail)
+        .EmailAddress()
+        .WithMessage("Please provide a valid email address");
+
+        RuleFor(dto => dto.PostalCode)
+        .Matches(@"^\d{2}-\d{3}$")
+        .WithMessage("Please provide a valid postal code (XX-XXX).");
+    }
+}
